7ce891279c512b74ab0520af1af1210ed3da6d67
Dimash Nsanbaev <nsanbaev.15@gmail.com>
2023-04-30 11:49:45 +0600

    Main Part with Board Square view

--- Sudoku/ContentView.swift
+++ Sudoku/ContentView.swift
@@ -5,17 +5,154 @@
 //  Created by Dimash Nsanbaev on 4/30/23.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @StateObject var board = BoardClass()
+    @State var isSolved: Bool = false
+    @State var selectedVal: Int = 0
+    @State var undoStack: [(from: Int, index: Int, value: Int)] = []
+    @State var redoStack: [(from: Int, index:Int, value:Int)] = []
+    
+    func getButtonColor(currentVal: Int, selectedVal: Int) -> Color {
+        if (currentVal == selectedVal) {
+            return Color.gray.opacity(1)
+        } else {
+            return Color.white.opacity(0.5)
         }
-        .padding()
+    }
+    
+    func setSelectedVal(selected: Int) -> Void {
+        selectedVal = selected
+    }
+    
+    var body: some View {
+        VStack(spacing:0) {
+            Text("Sudoku")
+                .bold()
+                .padding([.bottom], 20)
+                .font(.title3)
+            ForEach(0 ..< board.items.count / 9, content: { row in
+                HStack(spacing:0) {
+                    ForEach(0 ..< 9, content: {
+                        col in
+                        let index = row * 9 + col
+                        SquareView(data: board.items[index], onclick: {
+                            if (self.board.items[index].squareVal == selectedVal) {return}
+                            self.undoStack.append((self.board.items[index].squareVal, index, selectedVal))
+                            self.board.setSquare(index: index, newVal: selectedVal)
+                        })
+                    })
+                }
+            })
+            VStack(spacing:2) {
+                HStack(spacing:2) {
+                    ForEach(-2 ..< 4, content: { val in
+                        Button(action: {
+                            if (val >= 0) {
+                                self.setSelectedVal(selected: val)
+                                self.redoStack.removeAll()
+                            } else if (val == -2) {
+                                // undo action. So take it off top of undo stack, add it to redo stack
+                                if (undoStack.count == 0) {return}
+                                let lastAction = self.undoStack.removeLast()
+                                self.redoStack.append(lastAction)
+                                self.board.setSquare(index: lastAction.1, newVal: lastAction.0)
+                            } else if (val == -1) {
+                                // redo action. So take it off of redo stack and add it to undo stack
+                                if (redoStack.count == 0) {return}
+                                let lastAction = self.redoStack.removeLast()
+                                self.undoStack.append(lastAction)
+                                self.board.setSquare(index: lastAction.1, newVal: lastAction.2)
+                            }
+                        }, label: {
+                            if (val == 0) {
+                                Image("eraser")
+                                    .foregroundColor(Color.black)
+                                    .frame(width:40, height:40, alignment: .center)
+                                    .border(Color.gray)
+                                    .background(getButtonColor(currentVal: val, selectedVal: selectedVal))
+                            } else if (val == -1) {
+                                Image(systemName:"arrow.uturn.forward")
+                                    .foregroundColor(Color.black)
+                                    .frame(width:40, height:40, alignment: .center)
+                                    .border(Color.gray)
+                                    .background(getButtonColor(currentVal: val, selectedVal: selectedVal))
+                            } else if (val == -2) {
+                                Image(systemName:"arrow.uturn.backward")
+                                    .foregroundColor(Color.black)
+                                    .frame(width:40, height:40, alignment: .center)
+                                    .border(Color.gray)
+                                    .background(getButtonColor(currentVal: val, selectedVal: selectedVal))
+                            } else {
+                                Text(String(val))
+                                    .bold()
+                                    .foregroundColor(Color.black)
+                                    .frame(width:40, height:40, alignment: .center)
+                                    .border(Color.gray)
+                                    .background(getButtonColor(currentVal: val, selectedVal: selectedVal))
+                            }
+                        })
+                    })
+                }
+                HStack(spacing:2) {
+                    ForEach(4 ..< 10, content: { val in
+                        Button(action: {
+                            self.setSelectedVal(selected: val)
+                        }, label: {
+                            Text(String(val))
+                                .foregroundColor(Color.black)
+                                .bold()
+                                .frame(width:40, height:40, alignment: .center)
+                                .border(Color.gray)
+                                .background(getButtonColor(currentVal: val, selectedVal: selectedVal))
+                        })
+                    })
+                }
+
+            }
+            .padding([.top, .bottom], 20)
+            Button(action: {
+                self.board.revealOne()
+            }, label: {
+                Text("Reveal One")
+                    .foregroundColor(Color.white)
+                    .bold()
+                    .frame(alignment: .center)
+                    .padding(10)
+                    .border(Color.gray)
+                    .background(Color.gray)
+            })
+                .padding(.bottom, 2)
+            HStack(spacing:2) {
+                Button(action: {
+                    self.board.makeNewBoard()
+                }, label: {
+                    Text("New Game")
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .frame(alignment: .center)
+                        .padding(10)
+                        .border(Color.gray)
+                        .background(Color.gray)
+                })
+                Button(action: {
+                    self.board.solveBoard()
+                    self.board.numSteps = 0
+                }, label: {
+                    Text("View Solution")
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .frame(alignment: .center)
+                        .padding(10)
+                        .border(Color.gray)
+                        .background(Color.gray)
+                })
+            }
+        }
+        .navigationTitle("")
+
     }
 }
 
