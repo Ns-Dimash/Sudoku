aa65a5d3c3abc1b20a5d47e998d6283f9d4f9b58
Dimash Nsanbaev <nsanbaev.15@gmail.com>
2023-04-30 11:41:51 +0600

    FirstOpen

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Dimash Nsanbaev on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
